a65b45f96bfd0a1ea028ce7939c6bc1bc41b4391
vibhudawar <9vibhu@gmail.com>
2022-11-13 01:34:49 +0530

    deploying to netlify

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //now making new DB
-
 mongoose.connect(
   "mongodb+srv://admin-vibhu:%40Vibhu16@cluster0.4q8yj8k.mongodb.net/todolistDB"
 );
